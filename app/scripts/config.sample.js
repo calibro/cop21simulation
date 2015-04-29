579a7a69fee7f7f99352a96fe284b81841f625bf
uf0 <scnepa222@gmail.com>
2015-04-29 18:09:23 +0200

    added config file

---
+++ app/scripts/config.sample.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('cop21App')
+  .constant('TABLES_ID', 'insert the ID here')
+  .constant('DELEGATIONS_ID', 'insert the ID here')
+  .constant('EDGES_ID', 'insert the ID here');
